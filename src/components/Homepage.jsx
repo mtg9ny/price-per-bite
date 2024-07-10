b90cd9ca16925a3081734352763eb6dafa5a4233
Conroy Lee <ctlee264855@gmail.com>
2024-07-09 23:30:33 -0400

    Created search page

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,7 +1,30 @@
+import styles from '../styles/homepage.module.css'
 export default function HomePage() {
     return (
         <>
-            <div>This is the HomePage.</div>
+            <div className={styles.mainDiv}>
+                <div className={`${styles.header} ${styles.shadow}`}>
+                </div>
+
+                <div className={`${styles.inputContainer} ${styles.shadow}`} >
+                    <div className={styles.titleBlock}>
+                        <div className={styles.title}>Price Per Bite</div>
+                        <div className={styles.divider}></div>
+                    </div>
+                    <div>Search for your recipe here!</div>
+                    <div className={styles.searchBlock}>
+                        <div className={styles.searchContainer}>
+                            <input type="text" className={styles.styledInput} />
+                            <div className={styles.horizontalDivider}></div>
+                            <button type="submit" className={styles.submitArrow}>&gt;</button>
+                        </div>
+                        <div className={styles.bottomText}>Try searching for chicken parmesan, meatloaf, etc.</div>
+                    </div>
+                    <div>This website will automatically calculate the price of the recipe for you!</div>
+                </div>
+
+                <div className={styles.footer}></div>
+            </div>
         </>
     )
 }
---
+++ src/routes.jsx
@@ -0,0 +1,10 @@
+import Homepage from "./components/Homepage";
+
+const routes = [
+    {
+        path: "/",
+        element: <Homepage />,
+    },
+];
+
+export default routes;
