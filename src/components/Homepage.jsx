3c3a0a11a43d7d272e0339a8335681dd957892fc
Conroy Lee <ctlee264855@gmail.com>
2024-08-08 22:59:32 -0400

    Focus on input box on load and clean up colors

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,13 +1,29 @@
 import styles from '../styles/homepage.module.css'
 import useInput from '../hooks/useInput'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useEffect, useRef } from 'react';
 import Header from './Header.jsx'
 export default function HomePage() {
     const { input, setInput } = useInput();
+    const navigate = useNavigate();
+    const inputRef = useRef(null);
 
     const handleInputChange = (event) => {
         setInput(event.target.value);
     };
+
+    const handleKeyDown = (event) => {
+        if (event.key === "Enter") {
+            navigate('./search');
+        }
+    };
+
+    useEffect(() => {
+
+        if (inputRef.current) {
+            inputRef.current.focus();
+        }
+    }, []);
 
     return (
         <>
@@ -22,7 +38,7 @@ export default function HomePage() {
                     <div>Search for your recipe here!</div>
                     <div className={styles.searchBlock}>
                         <div className={styles.searchContainer}>
-                            <input type="text" className={styles.styledInput} value={input} onChange={handleInputChange} />
+                            <input type="text" className={styles.styledInput} value={input} onChange={handleInputChange} onKeyDown={handleKeyDown} ref={inputRef} />
                             <div className={styles.horizontalDivider}></div>
                             <Link to='./search' className={styles.submitArrow}>&gt;</Link>
                         </div>
@@ -33,7 +49,7 @@ export default function HomePage() {
 
                 <div className={styles.footer}>
                     <div>Created by Conroy Lee</div>
-                    <a href="https://github.com/mtg9ny" target="_blank">Github</a>
+                    <a href="https://github.com/mtg9ny" target="_blank">GitHub</a>
                 </div>
             </div>
         </>
--- src/components/SearchPage.jsx
+++ src/components/SearchPage.jsx
@@ -1,15 +1,23 @@
 import useFetchAPI from "../hooks/useFetchAPI";
 import Header from "./Header";
+import useInput from '../hooks/useInput'
 import Item from "./Item";
 import styles from '../styles/search-page.module.css'
 
 export default function SearchPage() {
+    const { input } = useInput();
     const { JSONObject } = useFetchAPI();
 
     const isDataLoaded = JSONObject && JSONObject.results && JSONObject.results.length > 0;
     return (
         <>
             <Header />
+
+            <div className={styles.headerContainer}>
+                <div className={styles.headerButton}>← Back to Search</div>
+                <div className={styles.headerText}>Search results for &quot;{input}&quot;</div>
+            </div>
+
             {isDataLoaded ? (
                 <div className={styles.itemContainer}>
                     {JSONObject.results.map((element, index) => (
