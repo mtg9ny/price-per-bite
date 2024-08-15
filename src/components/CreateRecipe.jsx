74b72bd21c02103f1c19bb2dfc9b783e24503689
Conroy Lee <ctlee264855@gmail.com>
2024-08-15 00:00:41 -0400

    Add input fields for create recipe except ingredients

--- src/components/CreateRecipe.jsx
+++ src/components/CreateRecipe.jsx
@@ -8,7 +8,28 @@ export default function CreateRecipe() {
             <Header />
             <div className={styles.bodyContent}>
                 <SideBar />
-                <div className={styles.individualMainContainer}>Create Recipe: not implemented.</div>
+                <div className={styles.individualMainContainer}>
+                    <div className={styles.title}>Create New Recipe:</div>
+                    <div className={styles.horizontalLine}></div>
+                    <form action="">
+                        <div className={styles.pair}>
+                            <label htmlFor="title">Title:</label>
+                            <input type="text" id="title" name="title" required />
+                        </div>
+
+                        <div className={styles.pair}>
+                            <label htmlFor="description">Description:</label>
+                            <textarea name="description" id="description" rows="2" cols="50" placeholder="Enter your description here..." maxLength="500" required></textarea>
+                        </div>
+
+                        <div className={styles.pair}>
+                            <label htmlFor="instructions">Instructions:</label>
+                            <textarea name="instructions" id="instructions" rows="2" cols="50" placeholder="Enter your instructions here..." maxLength="500" required></textarea>
+                        </div>
+                        <div className={styles.horizontalLine}></div>
+                        <button type="submit">Submit</button>
+                    </form>
+                </div>
             </div>
         </>
     )
